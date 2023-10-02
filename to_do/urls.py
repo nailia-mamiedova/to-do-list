708fd501bbf25cc57a44f1ee6b887f986c7f1a95
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-02 15:07:27 +0300

    Add URL patterns for task creation, updation and deletion

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, TaskCreateView, TaskUpdateView, TaskDeleteView
+
 
 urlpatterns = [
     path("", home_page, name="home-page"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("update/<int:pk>/", TaskUpdateView.as_view(), name="update-task"),
+    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="delete-task"),
 ]
 
 
