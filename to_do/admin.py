00bf903fbfcf28f8727aeeae94c15c0f1e7c0a9e
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-02 14:55:59 +0300

    Register models in admin

--- to_do/admin.py
+++ to_do/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task, Tag
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("is_done",)
+    ordering = ("is_done", "datetime")
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
