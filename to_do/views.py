6a5e9d38560e252a40f6956593050146c6d4f676
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-02 15:50:32 +0300

    Add CRUD view for the tag model

--- to_do/views.py
+++ to_do/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .models import Task
+from .models import Task, Tag
 
 
 def home_page(request):
@@ -25,3 +25,24 @@ class TaskUpdateView(generic.UpdateView):
 class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("to_do:home-page")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("to_do:tag-list")
