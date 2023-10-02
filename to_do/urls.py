e8d560bf66b812c2b6a2317b1df5a51c9f290444
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-02 15:50:57 +0300

    Add URL pattern for all tag views

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import home_page, TaskCreateView, TaskUpdateView, TaskDeleteView
+from .views import (
+    home_page,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 
 urlpatterns = [
@@ -7,6 +16,10 @@ urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="task-create"),
     path("update/<int:pk>/", TaskUpdateView.as_view(), name="update-task"),
     path("delete/<int:pk>/", TaskDeleteView.as_view(), name="delete-task"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="update-tag"),
+    path("tags/delete/<int:pk>/", TagDeleteView.as_view(), name="delete-tag"),
 ]
 
 
